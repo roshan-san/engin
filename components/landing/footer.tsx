96dcd4918e0b12bba2efc87d4d5951e88b8250e5
roshan <roshanjeyarubanr@gmail.com>
2025-04-25 13:53:47 -0700

    supbase codes

---
+++ components/landing/footer.tsx
@@ -0,0 +1,7 @@
+export default function footer() {
+  return (
+    <footer className="p-4 text-center text-sm text-muted-foreground border-t" >
+    © {new Date().getFullYear()} Engin. All rights reserved.
+     </footer>)
+}
+
