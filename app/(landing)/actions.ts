1a78bf4026840317601564c4c8a61d4090fc31fa
roshan <roshanjeyarubanr@gmail.com>
2025-06-01 19:24:46 +0530

    kit

--- app/(landing)/actions.ts
+++ app/(landing)/actions.ts
@@ -1,7 +1,9 @@
 "use server"
 
+import { profiles } from "@/lib/db/schema"
 import { createClient } from "@/lib/supabase/server"
 import { redirect } from "next/navigation"
+import { db } from "@/lib/db/drizzle"
 
 export async function handleOAuthLogin(provider: 'github' | 'google') {
   const supabase = await createClient()
@@ -37,4 +39,20 @@ export async function getUser() {
   const { data: { user } } = await supabase.auth.getUser()
   return user
 }
-
+export async function createProfile(data: {
+  avatar_url: string;
+  email: string;
+  username: string;
+  location: string;
+  user_type: 'Creator/Collaborator' | 'Investor' | 'Mentor';
+  employment_type: 'Full-Time' | 'Part-Time' | 'Contract';
+  full_name: string;
+  github_url?: string | null;
+  linkedin_url?: string | null;
+  bio?: string | null;
+  skills?: string[] | null;
+  interests?: string[] | null;
+}) {
+  const profile = await db.insert(profiles).values(data).returning()
+  return profile
+}
