72aaab5ef0d6edef0a994a5612b9a25562765bda
roshan <roshanjeyarubanr@gmail.com>
2025-06-02 15:12:20 +0530

    added suspense to landing page

--- app/(landing)/components/Header.tsx
+++ app/(landing)/components/Header.tsx
@@ -1,11 +1,14 @@
 import { ModeToggle } from '@/components/themes/theme-toggle'
+import { Suspense } from 'react'
 export default function Header() {
   return (
         <div className="w-screen p-4 flex items-center justify-between">
           <span className="text-4xl font-bold  uppercase tracking-wider text-primary">
             Engin
           </span>
-          <ModeToggle />   
+          <Suspense>
+            <ModeToggle />   
+          </Suspense>
         </div>
   )
 }
