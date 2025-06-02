c4bd736daaec2e72c51642f806c4bf4f684ee4f4
roshan <roshanjeyarubanr@gmail.com>
2025-06-02 21:26:07 +0530

    more schema changes

--- lib/db/tables/startups.ts
+++ lib/db/tables/startups.ts
@@ -3,14 +3,14 @@ import { profiles } from "./profiles";
 
 export const startups = pgTable("startups", {
   id: uuid("id").defaultRandom().primaryKey(),
-  name: text("name").notNull(),
-  location:text("location").notNull(),
-  description: text("description").notNull(),
-  problem: text("problem").notNull(),
-  solution: text("solution").notNull(),
-  teamSize: integer("team_size").notNull(),
-  patent: text("patent").notNull(),
-  funding: integer("funding").notNull(),
+  name: text("name").notNull().default(""),
+  location:text("location").notNull().default(""),
+  description: text("description").notNull().default(""),
+  problem: text("problem").notNull().default(""),
+  solution: text("solution").notNull().default(""),
+  teamSize: integer("team_size").notNull().default(1),
+  patent: text("patent").notNull().default("NA"),
+  funding: integer("funding").notNull().default(0),
   founderId: uuid("founder_id").references(() => profiles.id),
   created_at: timestamp("created_at").defaultNow().notNull(),
 }, (table) => [
