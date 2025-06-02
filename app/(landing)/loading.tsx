655fe556e487c0b064ae0592ba1b27fa85e9a4aa
roshan <roshanjeyarubanr@gmail.com>
2025-06-02 15:28:41 +0530

    implememted global spinner

--- app/(landing)/loading.tsx
+++ app/(landing)/loading.tsx
@@ -1,7 +1,6 @@
+import Spinner from "@/components/spinner";
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="w-8 h-8 border-4 border-primary border-t-transparent rounded-full animate-spin"></div>
-    </div>
+    <Spinner/>
   );
 }
---
+++ app/(platform)/dashboard/loading.tsx
@@ -0,0 +1,6 @@
+import Spinner from '@/components/spinner'
+export default function loading() {
+  return (
+    <Spinner/>
+  )
+}
