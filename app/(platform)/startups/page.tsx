24eb7563b90151c09ea34fd494f6e5b26bddb4ea
roshan <roshanjeyarubanr@gmail.com>
2025-06-03 15:01:15 +0530

    explore header

--- app/(platform)/startups/page.tsx
+++ app/(platform)/startups/page.tsx
@@ -1,4 +1,4 @@
-import SearchStartupsHeader from './components/SearchStartupsHeader'
+import SearchStartupsHeader from './components/ExploreHeader'
 export default function page() {
   return (
     <div className='h-full flex flex-col p-4 gap-10 border-2'>
