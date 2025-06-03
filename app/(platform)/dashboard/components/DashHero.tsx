06a76c37f07d87e94ebbdf7febd59747be4377d7
roshan <roshanjeyarubanr@gmail.com>
2025-06-03 14:41:55 +0530

    dashboard thing

---
+++ app/(platform)/dashboard/components/DashHero.tsx
@@ -0,0 +1,29 @@
+"use client"
+import React from 'react'
+import StartupCard from './StartupCard'
+import { Skeleton } from '@/components/ui/skeleton'
+import { useStartups } from '../hooks/useStartups'
+import Link from 'next/link'
+
+export default function DashHero() {
+    const { data: startups, isLoading } = useStartups()
+
+    if (isLoading) {
+        return (
+            <div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
+              {[...Array(3)].map((_, i) => (
+                <Skeleton key={i} className="h-[200px] w-full" />
+              ))}
+            </div>
+        )
+      }
+    
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
+        {startups?.map((startup) => (
+            <Link key={startup.id} href={`/startups/${startup.id}`}>
+                <StartupCard startup={startup} />
+            </Link>
+        ))}
+    </div>  )
+}
--- app/(platform)/dashboard/page.tsx
+++ app/(platform)/dashboard/page.tsx
@@ -1,34 +1,18 @@
-"use client"
 import DashHeader from './components/DashHeader'
+import DashHero from './components/DashHero'
 import StartupCard from './components/StartupCard'
 import { useStartups } from './hooks/useStartups'
 import { Skeleton } from '@/components/ui/skeleton'
 
 export default function Page() {
-  const { data: startups, isLoading } = useStartups()
 
-  if (isLoading) {
-    return (
-      <div className="h-full flex flex-col p-4 gap-10">
-        <DashHeader />
-        <div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
-          {[...Array(3)].map((_, i) => (
-            <Skeleton key={i} className="h-[200px] w-full" />
-          ))}
-        </div>
-      </div>
-    )
-  }
-
+ 
   return (
     <div className="h-full flex flex-col p-4 gap-10">
       <DashHeader/>
       {/* Cards */}
-      <div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
-        {startups?.map((startup) => (
-          <StartupCard key={startup.id} startup={startup} />
-        ))}
-      </div>
+      <DashHero/>
+      
     </div>
   )
 }
